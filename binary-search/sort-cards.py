d54c5dc6ed923897f356d2a8b2403f1ed6d94fa3
Fawaz441 <abdulsalamfawaz4@gmail.com>
2023-01-01 13:26:52 +0100

    first commit

---
+++ binary-search/sort-cards.py
@@ -0,0 +1,16 @@
+# sorting cards
+
+# represent the sequence of cards as a list
+
+def locate_card(cards, query):
+    pass
+
+
+# come up with some example inputs and output
+cards = [13, 11, 10.7, 4, 3, 1, 0]
+query = 7
+output = 3
+
+
+result = locate_card(cards, query)
+print(result == output)
